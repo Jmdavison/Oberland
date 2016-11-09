ecce9f5196201e1430799e9d06a1f1118e98ab14
jmdavison <jmdavison@wccnet.edu>
2016-11-09 18:54:43 -0500

    update on 11/9/2016

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,7 @@ function draw(){
   ctx.save();
   ctx.strokeStyle = 'rgba(0,0,0,0.8)';
   ctx.clearRect(0,0,600,600);
- //  ctx.beginPath();
- //  ctx.moveTo(300,300);
- //  ctx.bezierCurveTo(
- //    10,
- //    10,
- //    -10,
- //    -10,
- //    0,
- //    1+(Math.sin(time.getSeconds())*3)
- //  );
- // ctx.stroke();
+
  ctx.restore();
  window.requestAnimationFrame(draw);
 }
